1f9834195fd7b91160e81e83e6f58c5de4624094
artemitut <92166551+artemitut@users.noreply.github.com>
2022-02-17 21:36:18 +0200

    13

---
+++ index.js
@@ -0,0 +1,102 @@
+// 1
+// if (a==0){
+//     alert("True")
+// }
+// else{
+//     alert("False")
+// }
+
+// 2
+// if (a>0){
+//     alert("True")
+// }
+// else{
+//     alert("False")
+// }
+
+// 3
+// if (a<0){
+//     alert("True")
+// }
+// else{
+//     alert("False")
+// }
+
+// 4
+// if (a>=0){
+//     alert("True")
+// }
+// else{
+//     alert("False")
+// }
+
+// 5
+// if (a<=0){
+//     alert("True")
+// }
+// else{
+//     alert("False")
+// }
+
+// 6
+// if (a!=0){
+//     alert("True")
+// }
+// else{
+//     alert("False")
+// }
+
+// 7
+// if (a="test"){
+//     alert("True")
+// }
+// else{
+//     alert("False")
+// }
+
+// 8
+// if (a="1"){
+//     alert("True")
+// }
+// else{
+//     alert("False")
+// }
+
+// 9
+// if (test=true){
+//     alert("True")
+// }
+// else{
+//     alert("False")
+// }
+
+// 10
+// if (test!=true){
+//     alert("True")
+// }
+// else{
+//     alert("False")
+// }
+
+// 11
+// if (a>0 && a<5){
+//     alert("True")
+// }
+// else{
+//     alert("False")
+// }
+
+// 12
+// let a = 0
+// if (a==0 || a==2){
+//     a = a+7
+// }
+// else{
+//     a = a/10
+// }
+// alert(a)
+
+13
+if (a=1){
+    
+}
